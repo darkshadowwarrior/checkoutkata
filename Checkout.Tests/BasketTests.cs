e15462b6b3d8571795ff65efab0315293ee18d0c
Ian Richards <ianrichards@puresoftwaredesign.co.uk>
2022-02-13 21:13:39 +0000

    chore: added more tests

--- Checkout.Tests/BasketTests.cs
+++ Checkout.Tests/BasketTests.cs
@@ -19,6 +19,20 @@ namespace Checkout.Tests
         }
 
         [Test]
+        public void GivenThreeItemSToScan_ItemsCanBeAddedToBasketCart_ReturnsBasketTotalAs_()
+        {
+            var basket = new BasketService(new DiscountService());
+
+            basket.Scan(new Item { SKU = "A99", UnitPrice = 0.50 });
+            basket.Scan(new Item { SKU = "B15", UnitPrice = 0.30 });
+            basket.Scan(new Item { SKU = "C40", UnitPrice = 0.60 });
+
+            var basketTotal = basket.GetTotal();
+
+            Assert.That(basketTotal, Is.EqualTo(1.40));
+        }
+
+        [Test]
         public void GivenTwoItemToScan_ItemsCanBeAddedToBasketCart_ReturnsBasketTotalAs_80()
         {
             var basket = new BasketService(new DiscountService());
@@ -42,6 +56,84 @@ namespace Checkout.Tests
             var basketTotal = basket.GetTotal();
 
             Assert.That(basketTotal, Is.EqualTo(0.45));
+        }
+
+        [Test]
+        public void GivenThreeOfTheSameItemToScan_ItemsCanBeAddedToBasketCart_DiscountIdApplied_ReturnsBasketTotalAs_130()
+        {
+            var basket = new BasketService(new DiscountService());
+
+            basket.Scan(new Item { SKU = "A99", UnitPrice = 0.50 });
+            basket.Scan(new Item { SKU = "A99", UnitPrice = 0.50 });
+            basket.Scan(new Item { SKU = "A99", UnitPrice = 0.50 });
+
+            var basketTotal = basket.GetTotal();
+
+            Assert.That(basketTotal, Is.EqualTo(1.30));
+        }
+
+        [Test]
+        public void GivenASetOfTheItemsToScanInARandomOrder_ItemsCanBeAddedToBasketCart_DiscountIdApplied_ReturnsBasketTotalAs_235()
+        {
+            var basket = new BasketService(new DiscountService());
+
+            basket.Scan(new Item { SKU = "B15", UnitPrice = 0.30 });
+            basket.Scan(new Item { SKU = "A99", UnitPrice = 0.50 });
+            basket.Scan(new Item { SKU = "B15", UnitPrice = 0.30 });
+            basket.Scan(new Item { SKU = "A99", UnitPrice = 0.50 });
+            basket.Scan(new Item { SKU = "A99", UnitPrice = 0.50 });
+            basket.Scan(new Item { SKU = "C40", UnitPrice = 0.60 });
+
+            var basketTotal = basket.GetTotal();
+
+            Assert.That(basketTotal, Is.EqualTo(2.35));
+        }
+
+        [Test]
+        public void GivenASetOfTheItemsToScanInARandomOrder_ItemsCanBeAddedToBasketCart_DiscountIdApplied_ReturnsBasketTotalAs_470()
+        {
+            var basket = new BasketService(new DiscountService());
+
+            basket.Scan(new Item { SKU = "B15", UnitPrice = 0.30 });
+            basket.Scan(new Item { SKU = "A99", UnitPrice = 0.50 });
+            basket.Scan(new Item { SKU = "B15", UnitPrice = 0.30 });
+            basket.Scan(new Item { SKU = "A99", UnitPrice = 0.50 });
+            basket.Scan(new Item { SKU = "A99", UnitPrice = 0.50 });
+            basket.Scan(new Item { SKU = "C40", UnitPrice = 0.60 });
+
+            basket.Scan(new Item { SKU = "B15", UnitPrice = 0.30 });
+            basket.Scan(new Item { SKU = "A99", UnitPrice = 0.50 });
+            basket.Scan(new Item { SKU = "B15", UnitPrice = 0.30 });
+            basket.Scan(new Item { SKU = "A99", UnitPrice = 0.50 });
+            basket.Scan(new Item { SKU = "A99", UnitPrice = 0.50 });
+            basket.Scan(new Item { SKU = "C40", UnitPrice = 0.60 });
+
+            var basketTotal = basket.GetTotal();
+
+            Assert.That(basketTotal, Is.EqualTo(4.70));
+        }
+
+        [Test]
+        public void GivenASetOfTheItemsToScanInARandomOrder_ItemsCanBeAddedToBasketCart_DiscountIdApplied_ReturnsBasketTotalAs_445()
+        {
+            var basket = new BasketService(new DiscountService());
+
+            basket.Scan(new Item { SKU = "B15", UnitPrice = 0.30 });
+            basket.Scan(new Item { SKU = "A99", UnitPrice = 0.50 });
+            basket.Scan(new Item { SKU = "B15", UnitPrice = 0.30 });
+            basket.Scan(new Item { SKU = "A99", UnitPrice = 0.50 });
+            basket.Scan(new Item { SKU = "A99", UnitPrice = 0.50 });
+            basket.Scan(new Item { SKU = "C40", UnitPrice = 0.60 });
+
+            basket.Scan(new Item { SKU = "B15", UnitPrice = 0.30 });
+            basket.Scan(new Item { SKU = "A99", UnitPrice = 0.50 });
+            basket.Scan(new Item { SKU = "A99", UnitPrice = 0.50 });
+            basket.Scan(new Item { SKU = "A99", UnitPrice = 0.50 });
+            basket.Scan(new Item { SKU = "C40", UnitPrice = 0.60 });
+
+            var basketTotal = basket.GetTotal();
+
+            Assert.That(basketTotal, Is.EqualTo(4.55));
         }
     }
 }
