e719c4e5883874e444b1fb61c8fd776c5b224a33
Ian Richards <ianrichards@puresoftwaredesign.co.uk>
2022-02-13 20:30:14 +0000

    Feat: Added get total price for items scaned

--- Checkout.Tests/BasketTests.cs
+++ Checkout.Tests/BasketTests.cs
@@ -16,5 +16,18 @@ namespace Checkout.Tests
 
             Assert.That(itemCount, Is.EqualTo(1));
         }
+
+        [Test]
+        public void GivenTwoItemToScan_ItemsCanBeAddedToBasketCart_ReturnsBasketTotalAs_80()
+        {
+            var basket = new Basket();
+
+            basket.Scan(new Item { SKU = "A99", UnitPrice = 0.50 });
+            basket.Scan(new Item { SKU = "B15", UnitPrice = 0.30 });
+
+            var itemCount = basket.GetTotal();
+
+            Assert.That(itemCount, Is.EqualTo(0.80));
+        }
     }
 }
--- Checkout/Basket.cs
+++ Checkout/Basket.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Checkout.Models;
 
 namespace Checkout
@@ -20,6 +21,11 @@ namespace Checkout
         public int GetItemCount()
         {
             return _items.Count;
+        }
+
+        public double GetTotal()
+        {
+            return _items.Sum(o => o.UnitPrice);
         }
     }
 }
