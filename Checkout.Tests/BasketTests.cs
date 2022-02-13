ca829d1b9c1fe1a024ef98429df2ae6e466d6c5c
Ian Richards <ianrichards@puresoftwaredesign.co.uk>
2022-02-13 20:20:16 +0000

    Chore: Initial Commit

---
+++ Checkout.Tests/BasketTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace Checkout.Tests
+{
+    public class BasketTests
+    {
+        [Test]
+        public void InitialTest()
+        {
+            Assert.That(1, Is.EqualTo(1));
+        }
+    }
+}
